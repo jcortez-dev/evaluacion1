4bacd65761b6fcf2759f3aea4d99fb5f702decf7
Jorge Cortez <j.cortez01@ufromail.cl>
2023-09-25 19:21:47 -0300

    se agrega base del proyecto

---
+++ src/test/java/cl/ufro/dci/evaluacion1/Evaluacion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ufro.dci.evaluacion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
