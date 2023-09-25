63bde84284050e75f2be564e1f1ed8c76d5dc1b1
Jorge Cortez <j.cortez01@ufromail.cl>
2023-09-25 19:40:37 -0300

    se agregan anotaciones al modelo y se configura h2

--- src/main/java/cl/ufro/dci/evaluacion1/models/Console.java
+++ src/main/java/cl/ufro/dci/evaluacion1/models/Console.java
@@ -1,13 +1,18 @@
 package cl.ufro.dci.evaluacion1.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
 
+@Data
+@Entity
 public class Console {
+    @Id
+    @GeneratedValue
     private Integer id;
     private String name;
+
+    @OneToMany
     private List<Videogame> videogames;
 }
--- src/main/java/cl/ufro/dci/evaluacion1/models/Videogame.java
+++ src/main/java/cl/ufro/dci/evaluacion1/models/Videogame.java
@@ -1,8 +1,20 @@
 package cl.ufro.dci.evaluacion1.models;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
 import java.util.List;
 
+@Entity
+@Data
 public class Videogame {
+    @Id
+    @GeneratedValue
     private String name;
+
+    @ElementCollection
     private List<String>genres;
 }
